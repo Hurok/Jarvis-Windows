6409759767f5e1ddbf5ed0d6b6e2e0dea7e1784b
Vladislav Petrov <v.petrov@icmotion.me>
2017-11-14 13:38:41 +0600

    Added UDP Broadcast reciever and tray notification code

--- Jarvis WindowsClient/Program.cs
+++ Jarvis WindowsClient/Program.cs
@@ -1,18 +1,49 @@
-﻿using System;
+﻿using Jarvis_WindowsClient.network;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Jarvis_WindowsClient{
 
-    static class Program {
-        
+    class Program {
+
+        private static Program main;
+        private NotifyIcon trayIcon;
+        private JarvisNetwork network;
+
+        private void jarvisEvent(string jarvisEvent) {
+            Console.WriteLine(jarvisEvent);
+            main.trayIcon.BalloonTipText = jarvisEvent;
+            main.trayIcon.BalloonTipTitle = jarvisEvent;
+            main.trayIcon.ShowBalloonTip(5000);
+           // main.trayIcon.
+        }
+       
+
         [STAThread]
         static void Main(){
+
+            main = new Program();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
+
+
+
+            main.trayIcon = new NotifyIcon();
+            main.network = new JarvisNetwork(main.jarvisEvent);
+
+
+            main.trayIcon.Icon = SystemIcons.Application;
+            main.trayIcon.Visible = true;
+            //trayIcon.Icon
+            Application.Run();
         }
+
+
+
     }
 }
---
+++ Jarvis WindowsClient/network/JarvisNetwork.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Jarvis_WindowsClient.network {
+
+    public class JarvisNetwork {
+
+        private Action<string> eventCallback;
+
+        public void startListening(){
+
+            UdpClient serverSocket = new UdpClient();
+            serverSocket.Client.Bind(new IPEndPoint(IPAddress.Any, 53146));
+
+            IPEndPoint from = new IPEndPoint(0, 0);
+            
+            while (true){
+                var recvBuffer = serverSocket.Receive(ref from);
+                eventCallback(Encoding.UTF8.GetString(recvBuffer));
+                //Console.WriteLine(Encoding.UTF8.GetString(recvBuffer));
+            }
+        }
+
+        public JarvisNetwork(Action<string> callback) {
+
+            eventCallback = callback;
+
+            Thread networkThread = new Thread(startListening);
+            networkThread.Start();
+           
+        }
+
+        
+     
+
+
+
+
+    }
+
+
+}
